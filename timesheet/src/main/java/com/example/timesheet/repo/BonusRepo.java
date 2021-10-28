060342cc76c5abb4af98d88b3d5bdbd89e8ee1be
quang <quangngoc201197@gamil.com>
2021-10-28 17:24:54 +0700

    add entity timesheet

--- timesheet/src/main/java/com/example/timesheet/repo/BonusRepo.java
+++ timesheet/src/main/java/com/example/timesheet/repo/BonusRepo.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @Repository
 @Transactional
 public interface BonusRepo extends JpaRepository<Bouns, String>, JpaSpecificationExecutor<Bouns> {
